5ba470fa20b6a43f175b873e38cde6fc153c9ce7
Kai Schnittcher <kai.schnittcher@gmail.com>
2019-01-09 18:07:04 +0100

    Shelly Smoke hinzugefügt

---
+++ IPS-ShellySmoke/module.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+require_once __DIR__ . '/../libs/ShellyHelper.php';
+
+class IPS_ShellySmoke extends IPSModule
+{
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        $this->ConnectParent('{EE0D345A-CF31-428A-A613-33CE98E752DD}');
+
+        $this->RegisterVariableFloat('Shelly_Temperature', 'Temperature', '~Temperature');
+        $this->RegisterVariableBoolean('Shelly_Smoke', 'Smoke', '~Alert');
+        $this->RegisterVariableInteger('Shelly_Battery', 'Battery', '~Battery.100');
+        $this->RegisterPropertyString('MQTTTopic', '');
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+        $this->ConnectParent('{EE0D345A-CF31-428A-A613-33CE98E752DD}');
+        //Setze Filter für ReceiveData
+        $MQTTTopic = $this->ReadPropertyString('MQTTTopic');
+        $this->SetReceiveDataFilter('.*' . $MQTTTopic . '.*');
+    }
+
+    public function ReceiveData($JSONString)
+    {
+        $this->SendDebug('JSON', $JSONString, 0);
+        if (!empty($this->ReadPropertyString('MQTTTopic'))) {
+            $data = json_decode($JSONString);
+            // Buffer decodieren und in eine Variable schreiben
+            $Buffer = json_decode($data->Buffer);
+            $this->SendDebug('MQTT Topic', $Buffer->TOPIC, 0);
+
+            if (property_exists($Buffer, 'TOPIC')) {
+                if (fnmatch('*/sensor/temperature*', $Buffer->TOPIC)) {
+                    SetValue($this->GetIDForIdent('Shelly_Temperature'), $Buffer->MSG);
+                }
+                if (fnmatch('*/sensor/smoke*', $Buffer->TOPIC)) {
+                    SetValue($this->GetIDForIdent('Shelly_Smoke'), $Buffer->MSG);
+                }
+                if (fnmatch('*/sensor/battery*', $Buffer->TOPIC)) {
+                    SetValue($this->GetIDForIdent('Shelly_Battery'), $Buffer->MSG);
+                }
+            }
+        }
+    }
+}
