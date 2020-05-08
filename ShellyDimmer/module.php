eb294227712177b1f3ef0836ae699c3657dfc4d0
Kai <kai.schnittcher@gmail.com>
2020-05-08 14:25:41 +0200

    Setze Wert 0 wenn Dimmer aus ist

--- ShellyDimmer/module.php
+++ ShellyDimmer/module.php
@@ -151,6 +151,10 @@ class ShellyDimmer extends IPSModule
             $Payload['brightness'] = strval($value);
             $Payload['turn'] = 'on';
         }
+        if ($value == 0) {
+            $this->SetValue('Shelly_Brightness', 0);
+        }
+
         $Payload = json_encode($Payload);
         $this->sendMQTT($Topic, $Payload);
     }
