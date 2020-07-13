1ca76b8ec7b5084de26fe1d4a8d36afa2892e0b3
Kai Schnittcher <kai.schnittcher@gmail.com>
2020-07-13 11:44:51 +0200

    Version 4.2 Shelly Button1 hinzugefügt

---
+++ ShellyButton1/README_en.md
@@ -0,0 +1,16 @@
+# ShellyButton1
+   This module enables the integration of a ShellyButton1 in IP-Symcon.
+     
+   ## Table of Contents
+   1. [Configuration](#1-configuration)
+   2. [Functions](#2-functions)
+   
+   ## 1. Configuration
+   
+   Field        | Description
+   ------------ | -------------
+   MQTT Topic   | The Topic (shellybutton1-deviceid) of the ShellyButton1 is entered here. Currently, the following URL needs to be called for this: http://ShellyIP/settings The host name is found there. The host name is the DeviceID!
+   
+   ## 2. Functions
+   
+   Currently, there are no public functions available.
---
+++ ShellyButton1/module.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+require_once __DIR__ . '/../libs/ShellyHelper.php';
+require_once __DIR__ . '/../libs/VariableProfileHelper.php';
+require_once __DIR__ . '/../libs/MQTTHelper.php';
+
+class ShellyButton1 extends IPSModule
+{
+    use Shelly;
+    use VariableProfileHelper;
+    use MQTTHelper;
+
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+
+        $this->RegisterPropertyString('MQTTTopic', '');
+        $this->RegisterPropertyString('Device', '');
+        $this->RegisterVariableInteger('Shelly_Battery', $this->Translate('Battery'), '~Battery.100');
+
+        $this->RegisterProfileBooleanEx('Shelly.Reachable', 'Network', '', '', [
+            [false, 'Offline',  '', 0xFF0000],
+            [true, 'Online',  '', 0x00FF00]
+        ]);
+
+        $this->RegisterProfileIntegerEx('Shelly.Button1Input', 'ArrowRight', '', '', [
+            [0, $this->Translate('shortpush'),  '', 0x08f26e],
+            [1, $this->Translate('double shortpush'),  '', 0x07da63],
+            [2, $this->Translate('triple shortpush'),  '', 0x06c258],
+            [3, $this->Translate('longpush'),  '', 0x06a94d],
+        ]);
+
+        $this->RegisterVariableInteger('Shelly_Input', $this->Translate('Input'), 'Shelly.Button1Input');
+        $this->RegisterVariableBoolean('Shelly_Reachable', $this->Translate('Reachable'), 'Shelly.Reachable');
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+        //Setze Filter für ReceiveData
+        $MQTTTopic = $this->ReadPropertyString('MQTTTopic');
+        $this->SetReceiveDataFilter('.*' . $MQTTTopic . '.*');
+
+        if (($this->ReadPropertyString('Device') == 'shelly1pm')) {
+            $this->RegisterVariableFloat('Shelly_Power', $this->Translate('Power'), '~Watt.3680');
+            $this->RegisterVariableBoolean('Shelly_Overtemperature', $this->Translate('Overtemperature'), '');
+            $this->RegisterVariableFloat('Shelly_Temperature', $this->Translate('Temperature'), '~Temperature');
+            $this->RegisterVariableFloat('Shelly_Energy', $this->Translate('Energy'), '~Electricity');
+        }
+    }
+
+    public function ReceiveData($JSONString)
+    {
+        $this->SendDebug('JSON', $JSONString, 0);
+        if (!empty($this->ReadPropertyString('MQTTTopic'))) {
+            $data = json_decode($JSONString);
+
+            switch ($data->DataID) {
+                case '{7F7632D9-FA40-4F38-8DEA-C83CD4325A32}': // MQTT Server
+                    $Buffer = $data;
+                    break;
+                case '{DBDA9DF7-5D04-F49D-370A-2B9153D00D9B}': //MQTT Client
+                    $Buffer = json_decode($data->Buffer);
+                    break;
+                default:
+                    $this->LogMessage('Invalid Parent', KL_ERROR);
+                    return;
+            }
+            $this->SendDebug('MQTT Topic', $Buffer->Topic, 0);
+
+            if (property_exists($Buffer, 'Topic')) {
+                if (fnmatch('*/input/0', $Buffer->Topic)) {
+                    $Payload = json_decode($Buffer->Payload);
+                    $this->SendDebug('Input Payload', $Buffer->Payload, 0);
+                    switch ($Payload->event) {
+                        case 'S':
+                            $this->SetValue('Shelly_Input', 0);
+                            break;
+                        case 'SS':
+                            $this->SetValue('Shelly_Input', 1);
+                            break;
+                        case 'SSS':
+                            $this->SetValue('Shelly_Input', 2);
+                            break;
+                        case 'L':
+                            $this->SetValue('Shelly_Input', 3);
+                            break;
+                    }
+                }
+                if (fnmatch('*/online', $Buffer->Topic)) {
+                    $this->SendDebug('Online Payload', $Buffer->Payload, 0);
+                    switch ($Buffer->Payload) {
+                        case 'true':
+                            $this->SetValue('Shelly_Reachable', true);
+                            break;
+                        case 'false':
+                            $this->SetValue('Shelly_Reachable', false);
+                            break;
+                    }
+                }
+                if (fnmatch('*/sensor/battery*', $Buffer->Topic)) {
+                    $this->SendDebug('Battery Payload', $Buffer->Payload, 0);
+                    $this->SetValue('Shelly_Battery', $Buffer->Payload);
+                }
+            }
+        }
+    }
+}
