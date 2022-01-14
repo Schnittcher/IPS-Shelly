171cbcacb99bdc14de318f8babd23a57e3015c41
Kai Schnittcher <kai.schnittcher@gmail.com>
2022-01-14 10:41:29 +0100

    Vorbereitungen für die Instanz Shelly TRV

---
+++ ShellyTRV/module.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+require_once __DIR__ . '/../libs/ShellyHelper.php';
+require_once __DIR__ . '/../libs/VariableProfileHelper.php';
+require_once __DIR__ . '/../libs/MQTTHelper.php';
+
+class ShellyTRV extends IPSModule
+{
+    use Shelly;
+    use VariableProfileHelper;
+    use MQTTHelper;
+
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+
+        $this->RegisterPropertyString('MQTTTopic', '');
+
+        $this->RegisterProfileBooleanEx('Shelly.Reachable', 'Network', '', '', [
+            [false, 'Offline',  '', 0xFF0000],
+            [true, 'Online',  '', 0x00FF00]
+        ]);
+
+        $this->RegisterVariableBoolean('Shelly_Reachable', $this->Translate('Reachable'), 'Shelly.Reachable');
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+        //Setze Filter für ReceiveData
+        $MQTTTopic = $this->ReadPropertyString('MQTTTopic');
+        $this->SetReceiveDataFilter('.*' . MQTT_GROUP_TOPIC . '/' . $MQTTTopic . '.*');
+    }
+
+    public function RequestAction($Ident, $Value)
+    {
+        switch ($Ident) {
+            default:
+                $this->SendDebug('ReqestAction :: Invalid Ident', $ident, 0);
+                break;
+            }
+    }
+
+    public function ReceiveData($JSONString)
+    {
+        $this->SendDebug('ReceiveData :: JSON', $JSONString, 0);
+        if (!empty($this->ReadPropertyString('MQTTTopic'))) {
+            $data = json_decode($JSONString);
+
+            switch ($data->DataID) {
+                case '{7F7632D9-FA40-4F38-8DEA-C83CD4325A32}': // MQTT Server
+                    $Buffer = $data;
+                    break;
+                case '{DBDA9DF7-5D04-F49D-370A-2B9153D00D9B}': //MQTT Client
+                    $Buffer = json_decode($data->Buffer);
+                    break;
+                default:
+                    $this->LogMessage('Invalid Parent', KL_ERROR);
+                    return;
+            }
+            $this->SendDebug('ReceiveData :: MQTT Topic', $Buffer->Topic, 0);
+
+            if (property_exists($Buffer, 'Topic')) {
+            }
+        }
+    }
+}
