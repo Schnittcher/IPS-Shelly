5abdd2b12aba03b6eea030347caf2f3ec9dfbe87
Kai Schnittcher <kai.schnittcher@gmail.com>
2022-03-10 08:48:44 +0100

    Helper verschoben

--- ShellySmoke/module.php
+++ ShellySmoke/module.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 require_once __DIR__ . '/../libs/ShellyHelper.php';
-require_once __DIR__ . '/../libs/VariableProfileHelper.php';
+require_once __DIR__ . '/../libs/helper/VariableProfileHelper.php';
 
 class ShellySmoke extends IPSModule
 {
