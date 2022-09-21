6e4a500b21e312b6688dbdf8ad259d09e29bfd2b
Kai Schnittcher <kai.schnittcher@gmail.com>
2022-09-21 13:16:40 +0200

    Cover hinzugefügt

--- ShellyPro2/module.php
+++ ShellyPro2/module.php
@@ -6,32 +6,61 @@ require_once __DIR__ . '/../libs/ShellyModule.php';
 class ShellyPro2 extends ShellyModule
 {
     public static $Variables = [
-        ['State0', 'State 1', VARIABLETYPE_BOOLEAN, '~Switch', [], '', true, true],
+        ['State0', 'State 1', VARIABLETYPE_BOOLEAN, '~Switch', ['shellypro2', 'shellypro2pm'], 'relay', true, true],
+        ['Power0', 'Power 1', VARIABLETYPE_FLOAT, '~Watt.3680', ['shellypro2pm'], 'relay', false, true],
+        ['TotalEnergy0', 'Total Energy 1', VARIABLETYPE_FLOAT, '~Electricity', ['shellypro2pm'], 'relay', false, true],
+        ['Current0', 'Current 1', VARIABLETYPE_FLOAT, '~Ampere', ['shellypro2pm'], 'relay', false, true],
+        ['Voltage0', 'Voltage 1', VARIABLETYPE_FLOAT, '~Volt', ['shellypro2pm'], 'relay', false, true],
+        ['Overtemp0', 'Overtemp 1', VARIABLETYPE_BOOLEAN, '~Alert', [], 'relay', false, true],
+        ['Overpower0', 'Overpower 1', VARIABLETYPE_BOOLEAN, '~Alert', [], 'relay', false, true],
+        ['Overvoltage0', 'Overvoltage 1', VARIABLETYPE_BOOLEAN, '~Alert', [], 'relay', false, true],
 
-        ['Power0', 'Power 1', VARIABLETYPE_FLOAT, '~Watt.3680', ['shellypro2pm'], '', false, true],
-        ['TotalEnergy0', 'Total Energy1 ', VARIABLETYPE_FLOAT, '~Electricity', ['shellypro2pm'], '', false, true],
-        ['Current0', 'Current 1', VARIABLETYPE_FLOAT, '~Ampere', ['shellypro2pm'], '', false, true],
-        ['Voltage0', 'Voltage 1', VARIABLETYPE_FLOAT, '~Volt', ['shellypro2pm'], '', false, true],
+        ['State1', 'State 2', VARIABLETYPE_BOOLEAN, '~Switch', ['shellypro2', 'shellypro2pm'], 'relay', true, true],
+        ['Power1', 'Power 2', VARIABLETYPE_FLOAT, '~Watt.3680', ['shellypro2pm'], 'relay', false, true],
+        ['TotalEnergy1', 'Total Energy 2', VARIABLETYPE_FLOAT, '~Electricity', ['shellypro2pm'], 'relay', false, true],
+        ['Current1', 'Current 2', VARIABLETYPE_FLOAT, '~Ampere', ['shellypro2pm'], 'relay', false, true],
+        ['Voltage1', 'Voltage 2', VARIABLETYPE_FLOAT, '~Volt', ['shellypro2pm'], 'relay', false, true],
+        ['Overtemp1', 'Overtemp 2', VARIABLETYPE_BOOLEAN, '~Alert', ['shellypro2', 'shellypro2pm'], 'relay', false, true],
+        ['Overpower1', 'Overpower 2', VARIABLETYPE_BOOLEAN, '~Alert', ['shellypro2', 'shellypro2pm'], 'relay', false, true],
+        ['Overvoltage1', 'Overvoltage 2', VARIABLETYPE_BOOLEAN, '~Alert', ['shellypro2', 'shellypro2pm'], 'relay', false, true],
 
-        ['Overtemp0', 'Overtemp 1', VARIABLETYPE_BOOLEAN, '~Alert', [], '', false, true],
-        ['Overpower0', 'Overpower 1', VARIABLETYPE_BOOLEAN, '~Alert', [], '', false, true],
-        ['Overvoltage0', 'Overvoltage 1', VARIABLETYPE_BOOLEAN, '~Alert', [], '', false, true],
-
-        ['State1', 'State 2', VARIABLETYPE_BOOLEAN, '~Switch', [], '', true, true],
-
-        ['Power1', 'Power 2', VARIABLETYPE_FLOAT, '~Watt.3680', ['shellypro2pm'], '', false, true],
-        ['TotalEnergy1', 'Total Energy 2', VARIABLETYPE_FLOAT, '~Electricity', ['shellypro2pm'], '', false, true],
-        ['Current1', 'Current 2', VARIABLETYPE_FLOAT, '~Ampere', ['shellypro2pm'], '', false, true],
-        ['Voltage1', 'Voltage 2', VARIABLETYPE_FLOAT, '~Volt', ['shellypro2pm'], '', false, true],
-
-        ['Overtemp1', 'Overtemp 2', VARIABLETYPE_BOOLEAN, '~Alert', [], '', false, true],
-        ['Overpower1', 'Overpower 2', VARIABLETYPE_BOOLEAN, '~Alert', [], '', false, true],
-        ['Overvoltage1', 'Overvoltage 2', VARIABLETYPE_BOOLEAN, '~Alert', [], '', false, true],
+        ['CoverState', 'State', VARIABLETYPE_STRING, 'Shelly2ProPM.CoverState', ['shellypro2', 'shellypro2pm'], 'cover', true, true],
+        ['CoverRunningState', 'Running State', VARIABLETYPE_STRING, 'Shelly2ProPM.CoverRunningState', ['shellypro2', 'shellypro2pm'], 'cover', true, true],
+        ['CurrentPos', 'Current Position', VARIABLETYPE_INTEGER, '~Shutter', ['shellypro2pm'], 'cover', false, true],
+        ['TargetPos', 'Target Position', VARIABLETYPE_INTEGER, '~Shutter', ['shellypro2pm'], 'cover', true, true],
+        ['Power', 'Power', VARIABLETYPE_FLOAT, '~Watt.3680', ['shellypro2pm'], 'cover', false, true],
+        ['TotalEnergy', 'Total Energy', VARIABLETYPE_FLOAT, '~Electricity', ['shellypro2pm'], 'cover', false, true],
+        ['Current', 'Current', VARIABLETYPE_FLOAT, '~Ampere', ['shellypro2pm'], 'cover', false, true],
+        ['Voltage', 'Voltage', VARIABLETYPE_FLOAT, '~Volt', ['shellypro2pm'], 'cover', false, true],
+        ['Overtemp', 'Overtemp', VARIABLETYPE_BOOLEAN, '~Alert', [], 'cover', false, true],
+        ['Overpower', 'Overpower', VARIABLETYPE_BOOLEAN, '~Alert', [], 'cover', false, true],
+        ['Overvoltage', 'Overvoltage', VARIABLETYPE_BOOLEAN, '~Alert', [], 'cover', false, true],
 
         ['EventComponent', 'Event Component', VARIABLETYPE_STRING, '', [], '', false, true],
         ['Event', 'Event', VARIABLETYPE_STRING, '', [], '', false, true],
         ['Shelly_Reachable', 'Reachable', VARIABLETYPE_BOOLEAN, 'Shelly.Reachable', '', '', false, true]
     ];
+
+    public function Create()
+    {
+        parent::Create();
+        $this->RegisterPropertyString('DeviceType', '');
+        $this->RegisterProfileStringEx('Shelly2ProPM.CoverRunningState', 'Jalousie', '', '', [
+            ['open', $this->Translate('Open'), 'Jalousie', 0xFF0000],
+            ['opening ', $this->Translate('Opening'), 'Jalousie', 0xFF0000],
+            ['close', $this->Translate('Close'), 'Jalousie', 0x00FF00],
+            ['closing', $this->Translate('Closing'), 'Jalousie', 0x00FF00],
+            ['stop', $this->Translate('Stop'), 'Jalousie', 0xFF8800],
+            ['stopped', $this->Translate('Stopped'), 'Jalousie', 0xFF8800],
+            ['calibrating ', $this->Translate('Calibrating '), 'Jalousie', 0x8800FF]
+        ]);
+
+        $this->RegisterProfileStringEx('Shelly2ProPM.CoverState', 'Jalousie', '', '', [
+            ['open', $this->Translate('Open'), 'Jalousie', 0xFF0000],
+            ['close', $this->Translate('Close'), 'Jalousie', 0x00FF00],
+            ['stop', $this->Translate('Stop'), 'Jalousie', 0xFF8800]
+        ]);
+    }
 
     public function RequestAction($Ident, $Value)
     {
@@ -41,6 +70,14 @@ class ShellyPro2 extends ShellyModule
                 break;
             case 'State1':
                 $this->SwitchMode(1, $Value);
+                break;
+            case 'CoverState':
+                $this->CoverMode(0, $Value);
+                $this->SetValue('CoverState',$Value);
+                break;
+            case 'TargetPos':
+                $this->CoverGoToPosition(0, $Value);
+                $this->SetValue('TargetPos', $Value);
                 break;
             }
     }
@@ -71,11 +108,13 @@ class ShellyPro2 extends ShellyModule
                 }
                 if (fnmatch('*/events/rpc', $Buffer['Topic'])) {
                     if (array_key_exists('params', $Payload)) {
+                        //Events
                         if (array_key_exists('events', $Payload['params'])) {
                             $events = $Payload['params']['events'][0];
                             $this->SetValue('EventComponent', $events['component']);
                             $this->SetValue('Event', $events['event']);
                         }
+                        //Switch
                         for ($i = 0; $i <= 1; $i++) {
                             $switchIndex = 'switch:' . $i;
                             if (array_key_exists($switchIndex, $Payload['params'])) {
@@ -120,6 +159,52 @@ class ShellyPro2 extends ShellyModule
                                     }
                                 }
                             }
+                            //Cover
+                            if (array_key_exists('cover:0', $Payload['params'])) {
+                                $cover = $Payload['params']['cover:0'];
+                                if (array_key_exists('state', $cover)) {
+                                    $this->SetValue('CoverState', $cover['state']);
+                                }
+                                if (array_key_exists('apower', $cover)) {
+                                    $this->SetValue('Power', $cover['apower']);
+                                }
+                                if (array_key_exists('voltage', $cover)) {
+                                    $this->SetValue('Voltage', $cover['voltage']);
+                                }
+                                if (array_key_exists('current', $cover)) {
+                                    $this->SetValue('Current', $cover['current']);
+                                }
+                                if (array_key_exists('aenergy', $cover)) {
+                                    if (array_key_exists('total', $cover['aenergy'])) {
+                                        $this->SetValue('TotalEnergy', $cover['aenergy']['total'] / 1000);
+                                    }
+                                }
+                                if (array_key_exists('current_pos', $cover)) {
+                                    $this->SetValue('CurrentPos', $cover['current_pos']);
+                                }
+                                if (array_key_exists('errors', $cover)) {
+                                    $this->SetValue('Overtemp', false);
+                                    $this->SetValue('Overpower', false);
+                                    $this->SetValue('Overvoltage', false);
+                                    $errors = '';
+                                    foreach ($switch['errors'] as $key => $error) {
+                                        switch ($error) {
+                                            case 'overtemp':
+                                                $this->SetValue('Overtemp', true);
+                                                break;
+                                            case 'overpower':
+                                                $this->SetValue('Overpower', true);
+                                                break;
+                                            case 'Overvoltage':
+                                                $this->SetValue('Overvoltage', true);
+                                                break;
+                                            default:
+                                                $this->LogMessage('Missing Variable for Error State "' . $error . '"', KL_ERROR);
+                                                break;
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -127,14 +212,38 @@ class ShellyPro2 extends ShellyModule
         }
     }
 
-    private function SwitchMode(int $switch, bool $value)
+    private function SwitchMode(int $switchID, bool $value)
     {
         $Topic = $this->ReadPropertyString('MQTTTopic') . '/rpc';
 
         $Payload['id'] = 1;
         $Payload['src'] = 'user_1';
         $Payload['method'] = 'Switch.Set';
-        $Payload['params'] = ['id' => $switch, 'on' => $value];
+        $Payload['params'] = ['id' => $switchID, 'on' => $value];
+
+        $this->sendMQTT($Topic, json_encode($Payload));
+    }
+
+    private function CoverMode(int $coverID, bool $value)
+    {
+        $Topic = $this->ReadPropertyString('MQTTTopic') . '/rpc';
+
+        $Payload['id'] = 1;
+        $Payload['src'] = 'user_1';
+        $Payload['method'] = 'Cover.' . $value;
+        $Payload['params'] = ['id' => $coverID];
+
+        $this->sendMQTT($Topic, json_encode($Payload));
+    }
+
+    private function CoverGoToPosition(int $coverID, bool $value)
+    {
+        $Topic = $this->ReadPropertyString('MQTTTopic') . '/rpc';
+
+        $Payload['id'] = 1;
+        $Payload['src'] = 'user_1';
+        $Payload['method'] = 'Cover.GoToPosition';
+        $Payload['params'] = ['id' => $coverID, 'pos' => $value];
 
         $this->sendMQTT($Topic, json_encode($Payload));
     }
