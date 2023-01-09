74dea2afad3d09601aca49b02b6760a17dd0eb05
Kai Schnittcher <kai.schnittcher@gmail.com>
2023-01-09 11:45:54 +0100

    Fix: ShellyPlus2PM Status, Stromstärke, Volt, Gesamt Energie und Leistung wurde nicht richtig aktualisiert

--- ShellyPlus2PM/module.php
+++ ShellyPlus2PM/module.php
@@ -66,7 +66,7 @@ class ShellyPlus2PM extends ShellyModule
     {
         $this->SendDebug('JSON', $JSONString, 0);
         if (!empty($this->ReadPropertyString('MQTTTopic'))) {
-            $Buffer = json_decode($JSONString,true);
+            $Buffer = json_decode($JSONString, true);
 
             //Für MQTT Fix in IPS Version 6.3
             if (IPS_GetKernelDate() > 1670886000) {
