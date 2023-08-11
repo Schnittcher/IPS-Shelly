fe5dafa208b3f7f92cd24a4d7401eaa545fc804f
Kai Schnittcher <kai.schnittcher@gmail.com>
2023-08-11 14:22:05 +0200

    Shelly Pro 3EM Saldierung

--- ShellyPro3EM/module.php
+++ ShellyPro3EM/module.php
@@ -36,12 +36,6 @@ class ShellyPro3EM extends ShellyModule
         ['cTotalActRetEnergy', 'Phase C total active returned Energy', VARIABLETYPE_FLOAT, '~Electricity', [], '', false, true],
         ['totalActEnergy', 'Total active Energy', VARIABLETYPE_FLOAT, '~Electricity', [], '', false, true],
         ['totalActRetEnergy', 'Total active returned Energy', VARIABLETYPE_FLOAT, '~Electricity', [], '', false, true],
-
-        ['consumptionNetted', 'Consumption netted', VARIABLETYPE_FLOAT, '~Electricity', [], '', false, true],
-
-        ['energyFromGridNetted', 'Energy from Grid netted', VARIABLETYPE_FLOAT, '~Electricity', [], '', false, true],
-        ['energyToGridNetted', 'Energy to Grid netted', VARIABLETYPE_FLOAT, '~Electricity', [], '', false, true],
-
         ['Reachable', 'Reachable', VARIABLETYPE_BOOLEAN, 'Shelly.Reachable', '', '', false, true]
     ];
 
@@ -50,6 +44,18 @@ class ShellyPro3EM extends ShellyModule
         parent::Create();
         $this->RegisterPropertyFloat('TotalActiveEnergyOffset', 0);
         $this->RegisterPropertyFloat('TotalActRetEnergyOffset', 0);
+        $this->RegisterPropertyBool('Netting', false);
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+
+        $this->MaintainVariable('CurrentReturned', 'Current Returned', 2, '~Watt', 0, $this->ReadPropertyInteger('Netting'));
+        $this->MaintainVariable('CurrentImport', 'Current Import', 2, '~Watt', 0, $this->ReadPropertyInteger('Netting'));
+        $this->MaintainVariable('Import', 'Import', 2, '~Electricity', 0, $this->ReadPropertyInteger('Netting'));
+        $this->MaintainVariable('Returned', 'Returned', 2, '~Electricity', 0, $this->ReadPropertyInteger('Netting'));
     }
 
     public function ReceiveData($JSONString)
@@ -93,9 +99,10 @@ class ShellyPro3EM extends ShellyModule
                             $this->SetValue('totalCurrent', $em['total_current']);
                             $this->SetValue('totalActPower', $em['total_act_power']);
                             $this->SetValue('totalAprtPower', $em['total_aprt_power']);
+
+                            $this->Netting();
                         }
                         if (array_key_exists('emdata:0', $Payload['params'])) {
-                            $consumptionNetted = 0;
                             $emData = $Payload['params']['emdata:0'];
                             $this->SetValue('aTotalActEnergy', floatval($emData['a_total_act_energy']) / 1000);
                             $this->SetValue('aTotalActRetEnergy', floatval($emData['a_total_act_ret_energy']) / 1000);
@@ -106,19 +113,42 @@ class ShellyPro3EM extends ShellyModule
 
                             $this->SetValue('totalActEnergy', (floatval($emData['total_act']) / 1000) + $this->ReadPropertyFloat('TotalActiveEnergyOffset'));
                             $this->SetValue('totalActRetEnergy', (floatval($emData['total_act_ret']) / 1000) + $this->ReadPropertyFloat('TotalActRetEnergyOffset'));
-
-                            $consumptionNetted = ((floatval($emData['a_total_act_energy']) / 1000) + (floatval($emData['b_total_act_energy']) / 1000) + (floatval($emData['c_total_act_energy']) / 1000)) - ((floatval($emData['a_total_act_ret_energy']) / 1000) + (floatval($emData['b_total_act_ret_energy']) / 1000) + (floatval($emData['c_total_act_ret_energy']) / 1000));
-                            $this->SetValue('consumptionNetted', $consumptionNetted);
-
-                            if ($consumptionNetted > $this->GetValue('energyToGridNetted')) {
-                                $this->SetValue('energyFromGridNetted', $consumptionNetted);
-                            } else {
-                                $this->SetValue('energyToGridNetted', abs($consumptionNetted));
-                            }
                         }
                     }
                 }
             }
+        }
+    }
+
+    private function Netting()
+    {
+        $Leistung = $this->GetValue('totalActPower');
+
+        $varZwischenSpericherEinspeisung = IPS_GetVariable($this->GetIDForIdent('CurrentReturned'));
+        $varZwischenSpericherBezug = IPS_GetVariable($this->GetIDForIdent('CurrentImport'));
+
+        $ZwischenSpericherEinspeisung = $this->GetValue('CurrentReturned');
+        $ZwischenSpericherBezug = $this->GetValue('CurrentImport');
+
+        if ($ZwischenSpericherEinspeisung > 0) {
+            $zeit = ($varZwischenSpericherEinspeisung['VariableChanged'] - time()) / 3600;
+            $kwh = (abs($ZwischenSpericherEinspeisung) * abs($zeit)) / 1000;
+            SetValue($this->GetIDForIdent('Returned'), GetValue($this->GetIDForIdent('Returned')) + $kwh);
+        }
+
+        if ($ZwischenSpericherBezug > 0) {
+            $zeit = ($varZwischenSpericherBezug['VariableChanged'] - time()) / 3600;
+            $kwh = ($ZwischenSpericherBezug * abs($zeit)) / 1000;
+            SetValue($this->GetIDForIdent('Import'), GetValue($this->GetIDForIdent('Import')) + $kwh);
+        }
+
+        if ($Leistung < 0) {
+            SetValue($this->GetIDForIdent('CurrentReturned'), abs($Leistung));
+            SetValue($this->GetIDForIdent('CurrentImport'), 0);
+        }
+        if ($Leistung > 0) {
+            SetValue($this->GetIDForIdent('CurrentImport'), abs($Leistung));
+            SetValue($this->GetIDForIdent('CurrentReturned'), 0);
         }
     }
 }
