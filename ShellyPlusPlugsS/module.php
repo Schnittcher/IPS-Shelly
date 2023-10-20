ebddafabacf365963f38d318f639ad1e858196b8
Volker Hänsel <volker76@users.noreply.github.com>
2023-10-20 21:52:41 +0200

    Fixing bug with ShellyPlusPlug S not receiving temperature data
    taking receive data for topic /status/switch:0 and */status/* out of the bracket for /events/rpc

--- ShellyPlusPlugsS/module.php
+++ ShellyPlusPlugsS/module.php
@@ -30,7 +30,7 @@ class ShellyPlusPlugS extends ShellyModule
             }
     }
 
-    public function ReceiveData($JSONString)
+        public function ReceiveData($JSONString)
     {
         if (!empty($this->ReadPropertyString('MQTTTopic'))) {
             $Buffer = json_decode($JSONString, true);
@@ -46,6 +46,7 @@ class ShellyPlusPlugS extends ShellyModule
                 if (fnmatch('*/online', $Buffer['Topic'])) {
                     $this->SetValue('Reachable', $Payload);
                 }
+                
                 if (fnmatch('*/events/rpc', $Buffer['Topic'])) {
                     if (array_key_exists('params', $Payload)) {
                         if (array_key_exists('events', $Payload['params'])) {
@@ -95,32 +96,34 @@ class ShellyPlusPlugS extends ShellyModule
                                 }
                             }
                         }
-                        if (fnmatch('*/status/switch:0', $Buffer['Topic'])) {
-                            if (array_key_exists('output', $Payload)) {
-                                $this->SetValue('State', $Payload['output']);
-                            }
-                            if (array_key_exists('apower', $Payload)) {
-                                $this->SetValue('Power', $Payload['apower']);
-                            }
-                            if (array_key_exists('voltage', $Payload)) {
-                                $this->SetValue('Voltage', $Payload['voltage']);
-                            }
-                            if (array_key_exists('current', $Payload)) {
-                                $this->SetValue('Current', $Payload['current']);
-                            }
-                            if (array_key_exists('aenergy', $Payload)) {
-                                if (array_key_exists('total', $Payload['aenergy'])) {
-                                    $this->SetValue('TotalEnergy', $Payload['aenergy']['total'] / 1000);
-                                }
-                            }
+                    }
+                }
+                if (fnmatch('*/status/switch:0', $Buffer['Topic'])) {
+                    if (array_key_exists('output', $Payload)) {
+                        $this->SetValue('State', $Payload['output']);
+                    }
+                    if (array_key_exists('apower', $Payload)) {
+                        $this->SetValue('Power', $Payload['apower']);
+                    }
+                    if (array_key_exists('voltage', $Payload)) {
+                        $this->SetValue('Voltage', $Payload['voltage']);
+                    }
+                    if (array_key_exists('current', $Payload)) {
+                        $this->SetValue('Current', $Payload['current']);
+                    }
+                    if (array_key_exists('aenergy', $Payload)) {
+                        if (array_key_exists('total', $Payload['aenergy'])) {
+                            $this->SetValue('TotalEnergy', $Payload['aenergy']['total'] / 1000);
                         }
-                        //Temperatur ist immer vorhanden und sollte immer der selbe Wert sein.
-                        if (fnmatch('*/status/*', $Buffer['Topic'])) {
-                            if (array_key_exists('temperature', $Payload)) {
-                                if (array_key_exists('tC', $Payload['temperature'])) {
-                                    $this->SetValue('DeviceTemperature', $Payload['temperature']['tC']);
-                                }
-                            }
+                    }
+                }
+                        
+                //Temperatur ist immer vorhanden und sollte immer der selbe Wert sein.
+                if (fnmatch('*/status/*', $Buffer['Topic'])) {
+                            
+                    if (array_key_exists('temperature', $Payload)) {
+                        if (array_key_exists('tC', $Payload['temperature'])) {
+                            $this->SetValue('DeviceTemperature', $Payload['temperature']['tC']);
                         }
                     }
                 }
